9328728223cf4fc079e92b950810872658b2ca37
Abhilash Dash <abhilash200428@gmail.com>
2026-03-01 20:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
-  // your name here
+	var purse = {"q": 0, "d": 0, "n": 0, "p": 0};
+	var remaining = c;
+	if (remaining >= 25) {
+		purse.q += Math.floor(remaining / 25);
+		remaining = remaining % 25;
+	} if (remaining >= 10) {
+		purse.d += Math.floor(remaining / 10);
+		remaining = remaining % 10;
+	} if (remaining >= 5) {
+		purse.n += Math.floor(remaining / 5);
+		remaining = remaining % 5;
+	}
+	purse.p += remaining;
+	return purse;
 };
 
 // Do not the change the code below
